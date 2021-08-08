111599f437ab0ea51d5c9870436d05c95a19f064
franckalainai <franckalain.ai@gmail.com>
2021-08-08 23:08:40 +0000

    django introduction 1-12

---
+++ movieadmin/models.py
@@ -0,0 +1,17 @@
+from enum import auto
+from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields import DecimalField
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=36, blank=False, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+
+    price = DecimalField(default=0, max_digits=3, decimal_places=2)
+
+    published = models.DateTimeField(blank=True, default=None, null=True)
+    is_published = models.BooleanField(default=False)
+
+    cover = models.ImageField(upload_to='covers/', blank=True)
---
+++ movieadmin/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+class Another(View):
+    def get(self, request):
+        return HttpResponse('This is another function inside class')
+
+def first(request):
+    return HttpResponse('Fisrt message from views')
