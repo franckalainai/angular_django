bfa210d7d2b2e705968feb3b1237549eb719fd3e
franckalainai <franckalain.ai@gmail.com>
2021-08-09 10:18:22 +0000

    templates

--- movieadmin/views.py
+++ movieadmin/views.py
@@ -2,15 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from .models import Book
-
-
-class Another(View):
-    book = Book.objects.get(id=2)
-    output = f"we have {book.title} book with ID {book.id} in our database"
-
-    def get(self, request):
-        return HttpResponse(self.output)
-
+from django.shortcuts import render
 
 def first(request):
-    return HttpResponse('Fisrt message from views')
+    return render(request, 'myproject_temp.html')
