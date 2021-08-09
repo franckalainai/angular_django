9eee3dcd972823e90c98af75d4a942c689652f22
franckalainai <franckalain.ai@gmail.com>
2021-08-09 09:41:18 +0000

    model objects methods

--- movieadmin/views.py
+++ movieadmin/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .models import Book
+
 
 class Another(View):
+    book = Book.objects.get(id=2)
+    output = f"we have {book.title} book with ID {book.id} in our database"
+
     def get(self, request):
-        return HttpResponse('This is another function inside class')
+        return HttpResponse(self.output)
+
 
 def first(request):
     return HttpResponse('Fisrt message from views')
