446178f75e9feb94a3e87d98a5f4077e0d9cd067
franckalainai <65500799+franckalainai@users.noreply.github.com>
2021-10-11 17:59:26 +0000

    Delete movieadmin directory

--- movieadmin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from rest_framework import routers
-from .views import BookViewSet
-
-router = routers.DefaultRouter()
-router.register('Books', BookViewSet)
-urlpatterns = [
-    path('', include(router.urls))
-]
